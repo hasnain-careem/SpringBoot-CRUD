4194ec9b47e0b22627fe8ac0768e921621624902
hasnain-careem <hasnain.zafar@careem.com>
2025-05-26 13:32:27 +0500

    refactor: improved taskDTO

--- src/main/java/org/example/taskmanager/dto/TaskDto.java
+++ src/main/java/org/example/taskmanager/dto/TaskDto.java
@@ -1,5 +1,8 @@
 package org.example.taskmanager.dto;
 import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.example.taskmanager.model.TaskStatus;
@@ -8,9 +11,12 @@ import org.example.taskmanager.model.TaskStatus;
 @Setter
 public class TaskDto {
     private Long id;
+    @Size(max = 255, message = "Title must be less than 255 characters")
     private String title;
+    @Size(max = 1000, message = "Description must be less than 1000 characters")
     private String description;
     private TaskStatus status;
+    @FutureOrPresent(message = "Due date must be in the present or future")
     private LocalDateTime dueDate;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
